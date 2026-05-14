bded98a847cfcda615fcb87d3aaee84ae180aef4
trivedi-nitesh <nitesht@iitk.ac.in>
2026-05-14 13:53:38 +0530

    new experiments with LinExp

--- PRORP/driver.py
+++ PRORP/driver.py
@@ -13,6 +13,7 @@ OUTPUT_BASE_DIR = "/shared/output/"  # Workers write here
 M = 512
 p = 0.5
 
+
 def get_init_states(progname, k, M):
     INIT_STATES_MAP = {
         "MultiMode1": [f"(x == 0 && n < {k})"],
@@ -30,23 +31,31 @@ def get_init_states(progname, k, M):
         "Prinsys": ["(x == 0)"],
         "Detm": ["(count == 0)"],
         # "Fair": ["(count == 0)", "(count < 64)"],
-        "Fair": [ "(count < 64)"],
+        "Fair": ["(count < 64)"],
         "Mart": ["(c == 0 && b == 64 && rounds == 0)"],
         "GeoAr": ["(x == 0 && y == 0)"],
         "Bin0": [f"(x == 0 && y == 2 && n == {k})", f"(x < 64 && y == 2 && n == {k})"],
         "Bin1": [f"(x == 0 && n == {M - k/2})", f"(x < 32 && n == {M - k/2})"],
         "BinMod": [f"(x == 0 && n == {M - k/2})"],
-        "Bin2": [f"(x == 0 && y == 8 && n < {k/2})", f"(x < 32 && y == 8 && n < {k/2})"],
+        "Bin2": [
+            f"(x == 0 && y == 8 && n < {k/2})",
+            f"(x < 32 && y == 8 && n < {k/2})",
+        ],
         "BiasDir": ["(x == 0 && y == 0)", "(x == 1 && y == 1)"],
-        "DepRV": [f"(x == 0 && y == 0 && n == {k})", ],
-                #   f"(x == 8 && y == 16 && n == {k})"],
+        "DepRV": [
+            f"(x == 0 && y == 0 && n == {k})",
+        ],
+        #   f"(x == 8 && y == 16 && n == {k})"],
         "RevBin": [f"(x == {k/2} && z < 16)"],
         "Duel": ["(t == 0)", "(t == 1)"],
     }
     return INIT_STATES_MAP.get(progname, [])
 
+
 def ApproxInv(progname, iters, epsilon, eta, delta, mode, worker_id):
-    print(f"Running PRORP({progname},k={iters}, ε={epsilon}, η={eta}, δ={delta}), mode={mode}")
+    print(
+        f"Running PRORP({progname},k={iters}, ε={epsilon}, η={eta}, δ={delta}), mode={mode}"
+    )
     worker_output_dir = os.path.join(
         os.path.expanduser("~"), "ApproxInv_Output", f"worker_{worker_id}"
     )
@@ -58,57 +67,91 @@ def ApproxInv(progname, iters, epsilon, eta, delta, mode, worker_id):
         for k in k_values:
             init_states_list = get_init_states(progname, k, M)
             for init_states in init_states_list:
-                Progformula_path = os.path.join(CURRENT_PATH, "progformula/", "EXIST_progs.py")
+                Progformula_path = os.path.join(
+                    CURRENT_PATH, "progformula/", "EXIST_progs.py"
+                )
                 subprocess.run(
                     ["python3", Progformula_path, str(progname), str(num_bits), str(p)],
                     check=True,
                 )
                 subprocess.run(
                     [
-                        "python3", "initgen.py",
-                        "--progname", str(progname),
-                        "--init_states", str(init_states),
-                        "--num_bits", str(num_bits),
+                        "python3",
+                        "initgen.py",
+                        "--progname",
+                        str(progname),
+                        "--init_states",
+                        str(init_states),
+                        "--num_bits",
+                        str(num_bits),
                     ],
                     check=True,
                 )
-                Validifier_trigger_path = os.path.join(CURRENT_PATH, "src/Validifier/", "validifier.py")
-                Init_sampler_trigger_path = os.path.join(CURRENT_PATH, "src/Program_state_sampler/", "init_sampler.py")
-                TreeLearner_trigger_path = os.path.join(CURRENT_PATH, "src/TreeLearner/", "TreeLearner.py")
+                Validifier_trigger_path = os.path.join(
+                    CURRENT_PATH, "src/Validifier/", "validifier.py"
+                )
+                Init_sampler_trigger_path = os.path.join(
+                    CURRENT_PATH, "src/Program_state_sampler/", "init_sampler.py"
+                )
+                TreeLearner_trigger_path = os.path.join(
+                    CURRENT_PATH, "src/TreeLearner/", "TreeLearner.py"
+                )
                 subprocess.run(
                     [
-                        "python3", Init_sampler_trigger_path,
-                        "--progname", str(progname),
-                        "--epsilon", str(epsilon),
-                        "--eta", str(eta),
-                        "--delta", str(delta),
-                        "--iters", str(k),
-                        "--num_bits", str(num_bits),
-                        "--flag", str(mode),
+                        "python3",
+                        Init_sampler_trigger_path,
+                        "--progname",
+                        str(progname),
+                        "--epsilon",
+                        str(epsilon),
+                        "--eta",
+                        str(eta),
+                        "--delta",
+                        str(delta),
+                        "--iters",
+                        str(k),
+                        "--num_bits",
+                        str(num_bits),
+                        "--flag",
+                        str(mode),
                     ],
                     check=True,
                 )
                 cmd = [
-                    "python3", TreeLearner_trigger_path,
-                    "--progname", str(progname),
-                    "--epsilon", str(epsilon),
-                    "--eta", str(eta),
-                    "--delta", str(delta),
-                    "--iters", str(k),
-                    "--num_bits", str(num_bits),
-                    "--init_states", str(init_states),
-                    "--flag", str(mode),
+                    "python3",
+                    TreeLearner_trigger_path,
+                    "--progname",
+                    str(progname),
+                    "--epsilon",
+                    str(epsilon),
+                    "--eta",
+                    str(eta),
+                    "--delta",
+                    str(delta),
+                    "--iters",
+                    str(k),
+                    "--num_bits",
+                    str(num_bits),
+                    "--init_states",
+                    str(init_states),
+                    "--flag",
+                    str(mode),
                 ]
-                log_path = os.path.join(CURRENT_PATH, f"logs/logs_{num_bits}bits/{progname}_{epsilon}_{delta}.out")
+                log_path = os.path.join(
+                    CURRENT_PATH,
+                    f"logs/logs_{num_bits}bits/{progname}_{epsilon}_{delta}.out",
+                )
                 log_dir = os.path.dirname(log_path)
                 os.makedirs(log_dir, exist_ok=True)
                 with open(log_path, "a") as log_file:
                     log_file.write(f"\n--- Run at {datetime.now()} ---\n")
-                    subprocess.run(cmd, stdout=log_file, stderr=log_file, text=True, check=True)
+                    subprocess.run(
+                        cmd, stdout=log_file, stderr=log_file, text=True, check=True
+                    )
+
 
 if __name__ == "__main__":
-    
-    
+
     if len(sys.argv) == 7:
         progname = sys.argv[1]
         iters = int(sys.argv[2])
@@ -133,18 +176,18 @@ if __name__ == "__main__":
                 # ("Duel", 32, 0.05, 0.05, 0.1),
                 # ("Bin1", 32, 0.05, 0.05, 0.1),
                 # ("Prinsys", 32, 0.05, 0.05, 0.1),
-                ("LinExp",  64, 0.05, 0.05, 0.1, mode),
-                ("Fair", 64, 0.05, 0.05, 0.1, mode),
-                ("Sum0", 64, 0.05, 0.05, 0.1, mode),
+                ("LinExp", 32, 0.05, 0.05, 0.1, mode),
+                # ("Fair", 64, 0.05, 0.05, 0.1, mode),
+                # ("Sum0", 64, 0.05, 0.05, 0.1, mode),
                 # ("BigGeo0", 64, 0.05, 0.05, 0.1, mode),
                 # ("Detm", 32, 0.05, 0.05, 0.1),
-                ("DepRV", 64, 0.05, 0.05, 0.1, mode),
+                # ("DepRV", 64, 0.05, 0.05, 0.1, mode),
                 # ("Bin0", 32, 0.05, 0.05, 0.1),
                 # ("Bin2", 32, 0.05, 0.05, 0.1),
-                ("RevBin", 64, 0.05, 0.05, 0.1, mode),
+                # ("RevBin", 64, 0.05, 0.05, 0.1, mode),
                 # ("BiasDir", 32, 0.05, 0.05, 0.1),
                 # ("BigGeo1", 32, 0.05, 0.05, 0.1),
-                ("Mart", 64, 0.05, 0.05, 0.1, mode),
+                # ("Mart", 64, 0.05, 0.05, 0.1, mode),
                 # ("GeoAr", 32, 0.05, 0.05, 0.1),
                 # ("BigGeo2", 32, 0.05, 0.05, 0.1),
             ]
@@ -153,11 +196,6 @@ if __name__ == "__main__":
                 ApproxInv(*args, worker_id)
 
     print("All tasks completed.")
-
-
-            
-
-
 
 
 # if progname == "BigGeo0":
@@ -200,8 +238,3 @@ if __name__ == "__main__":
 #                                     #f"(x == {k/2} && z < 16)"]
 #             if progname == "Duel":
 #                 init_states_list = ["(t == 0)", "(t == 1)",]
-
-
-
-
-           
